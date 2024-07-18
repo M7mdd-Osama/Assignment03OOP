9ab605d2b03a22cf8df0ea6aacd1d8a6dff40412
Mohamed Osama Ali <ana20332183@gmail.com>
2024-07-18 18:40:39 +0300

    Polymorphism [Overloading]

--- Assignment03OOP/Program.cs
+++ Assignment03OOP/Program.cs
@@ -2,9 +2,39 @@
 {
     internal class Program
     {
+        public static int Sum(int X, int Y)
+        {
+            return X + Y;
+        }
+        public static double Sum(double X, double Y)
+        {
+            return X + Y;
+        }
+        public static int Sum(int X, int Y, int Z)
+        {
+            return X + Y + Z;
+        }
+        public static double Sum(int X, double Y)
+        {
+            return X + Y;
+        }
         static void Main(string[] args)
         {
-            
+            #region Polymorphism [Overloading]
+            //int Result = Sum(1, 2);
+            //Console.WriteLine(Result);
+            //Result = Sum(1, 2, 3);
+            //Console.WriteLine(Result);
+            //double Result02 = Sum(1.3, 2.5);
+            //Console.WriteLine(Result02);
+
+            Console.WriteLine(1);
+            Console.WriteLine(2.3);
+            Console.WriteLine(2.6M);
+            Console.WriteLine(1.3F);
+            Console.WriteLine("2");
+            Console.WriteLine('M');
+            #endregion
         }
     }
 }
