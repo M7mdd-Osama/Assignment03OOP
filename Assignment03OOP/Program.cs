c8c1882d375c7a65bf5942c9f8d04e0a1af5f6d9
Mohamed Osama Ali <ana20332183@gmail.com>
2024-07-18 21:02:42 +0300

    Binding

--- Assignment03OOP/Program.cs
+++ Assignment03OOP/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignment03OOP
+﻿using Assignment03OOP.Polymorphism_Overriding;
+
+namespace Assignment03OOP
 {
     internal class Program
     {
@@ -28,13 +30,30 @@
             //double Result02 = Sum(1.3, 2.5);
             //Console.WriteLine(Result02);
 
-            Console.WriteLine(1);
-            Console.WriteLine(2.3);
-            Console.WriteLine(2.6M);
-            Console.WriteLine(1.3F);
-            Console.WriteLine("2");
-            Console.WriteLine('M');
+            //Console.WriteLine(1);
+            //Console.WriteLine(2.3);
+            //Console.WriteLine(2.6M);
+            //Console.WriteLine(1.3F);
+            //Console.WriteLine("2");
+            //Console.WriteLine('M');
             #endregion
+
+            #region Polymorphism [Overriding]
+            //TypeA typeA = new TypeA(3);
+            //typeA.MyFun01();
+            //typeA.MyFun02();
+            //TypeB typeB = new TypeB(4, 5);
+            //typeB.MyFun01();
+            //typeB.MyFun02();
+            #endregion
+
+            #region Binding
+            //TypeA refBase = new TypeB(2,3);
+            //refBase.MyFun01();
+            //refBase.MyFun02();            
+            #endregion
+
+
         }
     }
 }
